fc8cf3a24138f7fa30ce5a7ab4bcd45318fca95d
Sasha <baglyuk.alexandr@gmail.com>
2019-03-11 17:45:49 +0300

    Использование промисов

---
+++ js/ajaxPromiseValuta.js
@@ -0,0 +1,43 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    'use strict';
+
+let inputRub = document.querySelector('.rub'),
+    inputUsd = document.querySelector('.usd');
+
+inputRub.addEventListener('input', () => {
+
+    function catchData() {
+
+        return new Promise(function(resolve, reject){
+            let request = new XMLHttpRequest();
+            request.open("GET", "js/current.json");
+        
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            request.send();
+        
+            request.onload = function() {
+                if(request.readyState === 4) {
+                        if(request.status == 200) {
+                            resolve(this.response)
+                        }
+                        else {
+                            reject();
+                        
+                        }
+                }
+            }
+        });
+    };
+
+    catchData()
+    .then(response => {
+        let data = JSON.parse(response);
+        inputUsd.value = inputRub.value / data.usd;
+    })
+    .catch(() => inputUsd.value = "Что-то пошло не так")
+
+
+});
+
+});
